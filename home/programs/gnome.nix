b0ef548f3bfee779a8ecad281771378a24a7e8bf
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-11-18 13:11:37 +0900

    chore: 日本語レイアウトを有効化

--- home/programs/gnome.nix
+++ home/programs/gnome.nix
@@ -2,7 +2,6 @@
   dconf.settings = {
     "org/gnome/desktop/input-sources" = {
       sources = [(lib.hm.gvariant.mkTuple ["xkb" "jp"])];
-      xkb-options = ["terminate:ctrl_alt_bksp"];
     };
   };
 }

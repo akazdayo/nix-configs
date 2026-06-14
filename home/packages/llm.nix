8a0a6737cf55c40b6199b6b4ac24418dcbea7454
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-06-14 18:59:48 +0900

    hotfix: lmstudioのnixが壊れてたので適当パッチを当てた

--- home/packages/llm.nix
+++ home/packages/llm.nix
@@ -1,17 +1,31 @@
 {
-  pkgs,
+  pkgs-unstable,
   pkgs-with-llm-agents,
   hostMeta,
   ...
 }:
 let
   isDesktop = hostMeta.hostName == "nixos";
+  lmstudio = pkgs-unstable.lmstudio.overrideAttrs (old: {
+    buildCommand =
+      builtins.replaceStrings
+        [
+          "/usr/share/icons/hicolor/0x0/apps/lm-studio.png"
+          "install -m 755 "
+          "patchelf --set-interpreter "
+        ]
+        [
+          "/resources/app/.webpack/Icon-512x512.png"
+          "true # install -m 755 "
+          "true # patchelf --set-interpreter "
+        ]
+        old.buildCommand;
+  });
 in
 {
   home.packages =
     (
       if isDesktop then
-        with pkgs;
         [
           lmstudio
         ]

f1733e6fa5039132bd8b756547ad0e03c7d04a77
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-06-21 17:06:49 +0900

    refactor: noctaliaをhomemanager管理に移した

--- home/programs/noctalia.nix
+++ home/programs/noctalia.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ inputs, pkgs, ... }:
 {
   # Noctalia Shell設定
   programs.noctalia-shell = {
     enable = true;
+    package = inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default;
     systemd.enable = false;
   };
 }
--- modules/nixos/system/packages.nix
+++ modules/nixos/system/packages.nix
@@ -1,13 +1,11 @@
 {
   pkgs,
   pkgs-unstable,
-  inputs,
   ...
 }:
 {
   environment.systemPackages = with pkgs; [
     vim
     pkgs-unstable.cloudflared
-    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 }

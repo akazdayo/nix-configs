d159b0eda9dd42647ab23f69abd7e39e4df10c50
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-07-06 00:03:03 +0900

    lockを更新

--- hosts/milk/host-data.nix
+++ hosts/milk/host-data.nix
@@ -47,9 +47,11 @@ in
 
   nix.settings.extra-trusted-substituters = [
     "https://cache.numtide.com"
+    "https://akazdayo-nixos-config.cachix.org"
   ];
   nix.settings.extra-trusted-public-keys = [
     "main:p1I0gblo5KOxd64LCmeOmENhGx/fRCVp5CS4aOQGY6w="
     "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    "akazdayo-nixos-config.cachix.org-1:beIgz4A31qNo0DBTEaTGbNy/xGuNJZZpH0n3BDc+JVk="
   ];
 }

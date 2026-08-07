d3d256e3563549d7abd09d840d7c8760b555c078
akazdayo <me@odango.app>
2026-08-07 13:59:15 +0900

    feat(darwin): cacheを有効化した

--- hosts/chiffon/default.nix
+++ hosts/chiffon/default.nix
@@ -6,6 +6,7 @@
     ../../modules/darwin/networking.nix
     ../../modules/darwin/user.nix
     ../../modules/darwin/secrets.nix
+    ../../modules/nixos/nix/cache.nix
   ];
 
   home-manager.users.${hostMeta.primaryUser} = {
@@ -29,6 +30,21 @@
       IdentityAgent = "none";
     };
     home.stateVersion = "25.11";
+  };
+
+  local = {
+    nix.cache = {
+      substituters = [
+        "https://cache.numtide.com"
+        "https://akazdayo-nixos-config.cachix.org"
+      ];
+      trustedPublicKeys = [
+        "main:p1I0gblo5KOxd64LCmeOmENhGx/fRCVp5CS4aOQGY6w="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+        "akazdayo-nixos-config.cachix.org-1:beIgz4A31qNo0DBTEaTGbNy/xGuNJZZpH0n3BDc+JVk="
+      ];
+      alwaysAllowSubstitutes = true;
+    };
   };
 
   system.stateVersion = 6;

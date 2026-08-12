ca3cefa901d1e728bf246e5a38032624875e6c49
akazdayo <me@odango.app>
2026-08-13 02:47:30 +0900

    feat: エンコードをいい感じにした

--- modules/nixos/jellyfin.nix
+++ modules/nixos/jellyfin.nix
@@ -1,7 +1,36 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  hardware.graphics = {
+    enable = true;
+    extraPackages = [ pkgs.intel-media-driver ];
+  };
+
+  users.users.jellyfin.extraGroups = [
+    "render"
+    "video"
+  ];
+
   services.jellyfin = {
     enable = true;
     openFirewall = false;
+    forceEncodingConfig = true;
+
+    hardwareAcceleration = {
+      enable = true;
+      type = "qsv";
+      device = "/dev/dri/renderD128";
+    };
+
+    transcoding = {
+      enableHardwareEncoding = true;
+      hardwareDecodingCodecs = {
+        h264 = true;
+        hevc = true;
+        hevc10bit = true;
+        mpeg2 = true;
+        vp9 = true;
+      };
+      hardwareEncodingCodecs.hevc = true;
+    };
   };
 }
